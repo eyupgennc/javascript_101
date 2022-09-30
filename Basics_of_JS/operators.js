28815692bbebc36558702b8c0de15242b64900fe
eyupgennc <y.eyup37@gmail.com>
2022-09-30 23:00:14 +0300

    operators are studied

---
+++ Basics_of_JS/operators.js
@@ -0,0 +1,30 @@
+let result;
+let a = 10,
+  b = 20,
+  c = 30;
+
+// Aritmetic Operators
+result = a + b;
+result = a - b;
+result = a * b;
+result = a / b;
+result = c % a;
+result = ++a;
+
+// Assignment Operators
+result = a;
+result -= a;
+result -= a;
+result *= a;
+result /= a;
+result %= a;
+
+// Comparison Operators
+result = a == b;
+result = a != b;
+result = 3 === "3"; //valur - type
+result = a > b;
+result = b > a;
+// Logical Operators
+
+console.log(result);
