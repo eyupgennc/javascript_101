cbf105193f61ae4f089dca3ba1bf4ab304602b82
eyupgennc <y.eyup37@gmail.com>
2022-09-30 22:56:55 +0300

    variables are studied

---
+++ Basics_of_JS/variables.js
@@ -0,0 +1,33 @@
+// var serverName = "eyupgennc.org";
+// console.log(serverName);
+
+// let server_Name;
+// server_Name = "https://google.com";
+
+// console.log(server_Name);
+
+// var productName = "Asus Keyboard";
+// let productPrice = "1500$";
+
+// console.log(typeof productName);
+// console.log(typeof productPrice);
+
+// let num1 = "10";
+// let num2 = "20";
+// console.log(Number(num1) + Number(num2));
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1.toString() + num2.toString());
+
+// let name1 = "eyup";
+// let surname = "genc";
+// console.log(name1 + " " + surname);
+
+// let examScore = 70;
+// let isSuccess = examScore > 60;
+// console.log(isSuccess);
+
+// let age;
+// console.log(age);
+// console.log(typeof age);
