dfab64473f143bd8811c80ebf778a5e46daee0c4
eyupgennc <y.eyup37@gmail.com>
2022-10-01 08:16:31 +0300

    conditionals are studied

---
+++ Basics_of_JS/conditionals.js
@@ -0,0 +1,20 @@
+// let userName = "eyup";
+// let password = "1234";
+
+// let isLoggedIn = userName == "eyup" && password == "1234";
+
+// if (isLoggedIn) {
+//   console.log("Login Successful");
+// } else {
+//   console.log("Login Failed");
+// }
+
+// if (userName == "eyup") {
+//   if (password == "1234") {
+//     console.log("Login Successful!");
+//   } else {
+//     console.log("Wrong Password!");
+//   }
+// } else {
+//   console.log("Wrong User Name!");
+// }
