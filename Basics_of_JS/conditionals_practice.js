ce377c6d3df8b361996ed969b521e8f3270cc80e
eyupgennc <y.eyup37@gmail.com>
2022-10-01 08:16:59 +0300

    a conditionals practice

---
+++ Basics_of_JS/conditionals_practice.js
@@ -0,0 +1,55 @@
+// let num = 20;
+
+// if (num < 50 && num > 20) {
+//   console.log("number is in the right range");
+// } else {
+//   console.log("sorry");
+// }
+
+//------------------------------------------------------------
+
+// let num = 3;
+
+// if (num % 2 == 1 && num > 0) {
+//   console.log("yeah buddy");
+// } else {
+//   console.log("sorry");
+// }
+
+//------------------------------------------------------------
+
+let x = 30,
+  y = 20,
+  z = 5;
+
+// if (x > y && x > z && y > z) {
+//   console.log("x > y > z");
+// } else if (x > y && x > z && z > y) {
+//   console.log("x > z > y");
+// } else if (y > x && y > z && x > z) {
+//   console.log("y > x > z");
+// } else if (y > x && y > z && z > x) {
+//   console.log("y > z > x");
+// } else if (z > x && z > y && y > x) {
+//   console.log("z > y > x");
+// } else if (z > x && z > y && x > y) {
+//   console.log("z > x > y");
+// } else {
+//   console.log("nums are equal");
+// }
+
+//------------------------------------------------------------
+
+let v1 = 10;
+let v2 = 10;
+let f = 70;
+
+avarage = ((v1 + v2) / 2) * 0.6 + 20 * 0.4;
+
+if ((avarage > 50 && f > 50) || f >= 70) {
+  console.log("avarage = " + avarage);
+  console.log("yeah buddy");
+} else {
+  console.log("av = " + avarage);
+  console.log("nope");
+}
