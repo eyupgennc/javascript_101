117f46a7071055914bd154eaf6eb364a5c23ab38
eyupgennc <y.eyup37@gmail.com>
2022-10-02 16:20:53 +0300

    string methods are studied

---
+++ Basics_of_JS/string_methods.js
@@ -0,0 +1,27 @@
+course = "This is probably the greatest thing that's ever happened in my life.";
+
+final = course.toLowerCase();
+final = course.toUpperCase();
+final = course.length;
+
+final = course[1];
+final = course.slice(0, 4);
+final = course.slice(10);
+final = course.slice(-2);
+final = course.slice(-10, -2);
+
+final = course.substring(0, 6);
+final = course.substring(10);
+
+final = course.replace("This", "That");
+
+final = course.trim();
+final = course.trimEnd();
+final = course.trimStart();
+
+final = course.indexOf("probably");
+
+final = course.split(" ");
+final = course.split(" ")[4];
+final = course.split(" ")[5];
+console.log(final);
